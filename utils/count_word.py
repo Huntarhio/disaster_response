6469724a92762926b418cf5ef12e3033ce176b78
gboyega.a@credpal.com <gboyega.a@credpal.com>
2020-04-10 17:43:12 +0100

    adding docstring to the most_common words function

--- utils/count_word.py
+++ utils/count_word.py
@@ -6,14 +6,34 @@ from collections import Counter
 import pandas as pd
 import numpy as np
 
-def count_word(data):
-    text = ''.join(data)
-    new_text = re.sub('[^a-zA-Z]+', ' ', text)
-    word_tokens = word_tokenize(''.join(new_text))
+import re
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english')) 
+from collections import Counter
+import pandas as pd
+import numpy as np
 
+def count_word(data):
+    """
+    this function counts the words in the all messages in the data and returns the most common words
+
+    PARAMETER:
+        data (series): the series of all messages in the data
+
+    RETURN:
+        count_list (list): list of the counts of the most common words
+        words_list (list): list of the most common words
+    """
+    text = ''.join(data)  # joining all the rows into one
+    new_text = re.sub('[^a-zA-Z]+', ' ', text)  # extracting on the words
+    word_tokens = word_tokenize(''.join(new_text))  #tokenizing into items
+
+
+    # removing all stopwords 
     filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words] 
     
-    counts = (Counter(filtered_sentence))
+    counts = (Counter(filtered_sentence))  # counting the words and saving to dictionary
     counts = dict(counts)
 
     words = counts.keys()
